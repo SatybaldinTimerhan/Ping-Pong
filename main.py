3b5a40d1b0b33d3a2e1f3f4812fea828b58abed7
SergeyKalutsky <skalutsky@gmail.com>
2024-04-27 09:47:25 +0300

    made main window

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pygame
+
+FPS = 30
+pygame.init()
+window  = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Ping-pong")
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+            
+    pygame.display.update()
+    clock.tick(FPS)
